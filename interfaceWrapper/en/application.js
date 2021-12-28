dd4ffd3d439f4662912ae407b7e430f86be2b3fc
Kirillbiliashov <73443541+Nutulut210@users.noreply.github.com>
2021-12-28 10:22:33 +0200

    interface wrapper tasks are implemented

--- interfaceWrapper/en/application.js
+++ interfaceWrapper/en/application.js
@@ -9,4 +9,22 @@ function timerEvent() {
 }
 
 // Create timer
-setTimeout(timerEvent, 1000);
+const write = (filename, data) => fs.writeFile(filename, data, (err) => {
+  for (let i = 0; i < 1000000; i++) { }
+  console.log('write successfully completed');
+});
+
+const read = (filename) => fs.readFile(filename, 'utf8', (err, data) => {
+  for (let i = 0; i < 500000; i++) { }
+  console.log('read successfully completed');
+});
+
+for (let i = 0; i < 10; i++) {
+  const msec = Math.floor(Math.random() * 10000);
+  timers.setTimeout(write, msec, './someData.txt', 'Adding some data...');
+}
+
+for (let i = 0; i < 10; i++) {
+  const msec = Math.floor(Math.random() * 10000);
+  timers.setTimeout(read, msec, './README.md');
+}
--- interfaceWrapper/en/framework.js
+++ interfaceWrapper/en/framework.js
@@ -5,31 +5,98 @@
 global.api = {};
 api.fs = require('fs');
 api.vm = require('vm');
+api.timers = require('timers');
+
+const EventEmitter = require('events');
+const ee = new EventEmitter();
+
+const stats = {
+  fnCalls: 0,
+  cbCalls: 0,
+  execTime: 0,
+  cbTime: 0,
+  sizeRead: 0,
+  sizeWritten: 0,
+  writeTime: 0,
+  readTime: 0
+}
+
+const interfaceWrapper = (anInterface) => {
+  const clone = {};
+  for (const key in anInterface) {
+    clone[key] = wrapFunction(key, anInterface[key]);
+  }
+  return clone
+}
+
+const wrapFunction = (fnName, fn) => (...args) => {
+  const lastArg = args[args.length - 1];
+  const fnStart = new Date().getTime();
+  ee.emit('fnCall', fnName);
+  if (typeof lastArg === 'function') {
+    const cb = (err, data) => {
+      if (err) return;
+      if (fnName.includes('read')) ee.emit('read', data.length, readStart);
+      else if (fnName.includes('write')) ee.emit('write', args[1].length, writeStart);
+      const cbStart = new Date().getTime();
+      ee.emit('cbStart', fnName);
+      lastArg(err, data);
+      ee.emit('cbEnd', cbStart);
+      ee.emit('fnEnd', fnStart);
+    }
+    args[args.length - 1] = cb;
+    const readStart = new Date().getTime();
+    const writeStart = new Date().getTime();
+    fn(...args);
+  } else {
+    const res = fn(...args);
+    ee.emit('fnEnd', fnStart);
+    return res;
+  }
+}
+
+ee.on('fnCall', (name) => {
+  console.log(`function call: ${name}`);
+  stats.fnCalls++;
+});
+ee.on('fnEnd', (start) => {
+  const diff = new Date().getTime() - start;
+  stats.execTime = stats.fnCalls === 1 ? diff : (stats.execTime * (stats.fnCalls - 1) + diff) / stats.fnCalls;
+});
+ee.on('cbStart', (name) => {
+  stats.cbCalls++;
+  console.log(`callback call: ${name}`);
+
+});
+ee.on('read', (size, start) => {
+  stats.sizeRead += size;
+  const diff = new Date().getTime() - start;
+  stats.readTime = stats.cbCalls === 0 ? diff : (stats.readTime * stats.cbCalls + diff) / (stats.cbCalls + 1);
+});
+ee.on('write', (size, start) => {
+  stats.sizeWritten += size;
+  const diff = new Date().getTime() - start;
+  stats.writeTime = stats.cbCalls === 0 ? diff : (stats.writeTime * stats.cbCalls + diff) / (stats.cbCalls + 1);
+});
+ee.on('cbEnd', (start) => {
+  const diff = new Date().getTime() - start;
+  console.dir({diff})
+  stats.cbTime = stats.cbCalls === 1 ? diff : (stats.cbTime * (stats.cbCalls - 1) + diff) / stats.cbCalls;
+});
+
 
 // Create a hash for application sandbox
 const context = {
   module: {},
   console,
+  Math,
   // Forward link to fs API into sandbox
-  fs: api.fs,
+  fs: interfaceWrapper(api.fs),
+  //fs: interfaceWrapper(api.fs);
   // Wrapper for setTimeout in sandbox
-  setTimeout: (callback, timeout) => {
-    // Logging all setTimeout calls
-    console.log(
-      'Call: setTimeout, ' +
-      'callback function: ' + callback.name + ', ' +
-      'timeout: ' + timeout
-    );
-    setTimeout(() => {
-      // Logging timer events before application event
-      console.log('Event: setTimeout, before callback');
-      // Calling user-defined timer event
-      callback();
-      console.log('Event: setTimeout, after callback');
-    }, timeout);
-  }
+  timers: api.timers,
 };
-
+//timeout: (msec, cb) => setTimeout(cb, msec) 
 // Turn hash into context
 context.global = context;
 const sandbox = api.vm.createContext(context);
@@ -41,3 +108,4 @@ api.fs.readFile(fileName, 'utf8', (err, src) => {
   const script = api.vm.createScript(src, fileName);
   script.runInNewContext(sandbox);
 });
+api.timers.setInterval(() => console.dir({ stats }), 30000);
